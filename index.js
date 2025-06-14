1c283d3e3198b250c07b9d01099b388e899a7f86
Kin1922 <faishalpython@gmail.com>
2025-06-14 23:16:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,24 +3,28 @@ const app = express();
 const path = require('path');
 const port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname));
+const links = {};
+
+app.use(express.static(__dirname)); // biar bisa load index.html
+app.use(express.json()); // biar bisa parsing JSON body
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-const links = {};
-
-app.post('/shorten', express.json(), (req, res) => {
-  const id = Math.random().toString(36).substr(2,5);
+app.post('/shorten', (req, res) => {
+  const id = Math.random().toString(36).substr(2, 5);
   links[id] = req.body.url;
-  res.json({ short: `${req.headers.host}/${id}` });
+  const shortUrl = `${req.protocol}://${req.get('host')}/${id}`;
+  res.json({ short: shortUrl });
 });
 
 app.get('/:id', (req, res) => {
   const url = links[req.params.id];
   if (url) return res.redirect(url);
-  res.status(404).send('Link gak ada bro');
+  res.status(404).send('Link gak ada bre!');
 });
 
-app.listen(port, () => console.log(`Running on port ${port}`));
+app.listen(port, () => {
+  console.log(`Server jalan di port ${port}`);
+});
